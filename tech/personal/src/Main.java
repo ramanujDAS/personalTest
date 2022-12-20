772b628d20caef52f4d274222f84c2bcf275e278
ramanuj2 <ramanuj@fplabs.tech>
2022-12-20 23:36:05 +0530

    Main

--- tech/personal/src/Main.java
+++ tech/personal/src/Main.java
@@ -1,8 +1,26 @@
 package  tech.personal.src;
+
+import com.sun.javafx.binding.Logging;
+
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws ClassNotFoundException {
 
+    printClassLoader();
     }
+
+     public static void printClassLoader() throws ClassNotFoundException {
+         System.out.println("Classloader of Logging:"
+                 + Logging.class.getClassLoader());
+
+         System.out.println("Classloader of ArrayList:"
+                 + ArrayList.class.getClassLoader());
+
+         CustomClassLoader customClassLoader = new CustomClassLoader();
+
+         customClassLoader.findClass("/Users/ramanujsingh/personalTest/out/production/personalTest/tech/personal/src/MyClass.class");
+
+     }
 }
--- tech/personal/src/MyClass.java
+++ tech/personal/src/MyClass.java
@@ -1,4 +1,9 @@
 package tech.personal.src;
 
 public class MyClass {
+
+    public MyClass()
+    {
+        System.out.println("custom Class Loader");
+    }
 }
